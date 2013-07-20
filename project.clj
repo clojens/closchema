3e52c0387607c6e619f08a74af1052ce383e1feb
supersymmetry <maildumpert@gmail.com>
2013-07-20 07:16:32 +0200

    updated references and < 80 col line width

--- project.clj
+++ project.clj
@@ -1,6 +1,3 @@
 (defproject org.clojars.hms/closchema "0.0.2"
   :description "Implements the JSON Schema specification."
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[swank-clojure "1.3.2"]]
-  :repositories {"releases"  "http://192.168.0.7:8080/archiva/repository/internal/"
-                 "snapshots" "http://192.168.0.7:8080/archiva/repository/snapshots/"})
+  :dependencies [[org.clojure/clojure "1.5.1"]])
--- src/closchema/core.clj
+++ src/closchema/core.clj
@@ -1,10 +1,11 @@
 (ns closchema.core
-  "This is JSON Schema in Clojure. See http://tools.ietf.org/html/draft-zyp-json-schema-02
- Main purposed is to allow object validation, but schema metadata can be used for exposing contracts as well."
+  "This is JSON Schema in Clojure.
+  See http://tools.ietf.org/html/draft-zyp-json-schema-02
+  Main purposed is to allow object validation, but schema metadata can be used
+  for exposing contracts as well."
   (:use clojure.walk clojure.template)
   (:require [clojure.set :as set])
   (:import [java.util ArrayList Stack]))
-
 
 (def ^{:doc "Allow validation errors to be captured." :dynamic true}
      *validation-context* nil)
@@ -13,26 +14,27 @@
        :dynamic true}
      *parent* nil)
 
-
 (def ^{:doc "Default processing just outputs a boolean return." :dynamic true}
   process-errors
   (fn [errors] (= (count errors) 0)))
 
-
 (defmacro with-validation-context
-  "Defines a binding to allow access to the root object and to enable invalidations to be captured. This strategy removes the need of raising exceptions at every single invalid point, and allows context information to be used when reporting about errors. Nested contexts are just ignored."
+  "Defines a binding to allow access to the root object and to enable
+  invalidations to be captured. This strategy removes the need of raising
+  exceptions at every single invalid point, and allows context information to
+  be used when reporting about errors. Nested contexts are just ignored."
   [& body]
   `(let [body# #(do ~@body
-                    (process-errors (:errors *validation-context*)))]
+                  (process-errors (:errors *validation-context*)))]
      (if-not *validation-context*
        (binding [*validation-context* {:errors (ArrayList.)
                                        :path   (Stack.)}]
          (body#))
        (body#))))
 
-
 (defmacro walk-in
-  "Step inside a relative path, from a previous object. This information is useful for reporting."
+  "Step inside a relative path, from a previous object. This information is
+  useful for reporting."
   [parent rel-path & body]
   `(binding [*parent* ~parent]
      (if-let [^Stack path# (:path *validation-context*)]
@@ -42,11 +44,11 @@
          (.pop path#))
        (do ~@body))))
 
-
 (defmacro invalid
   "Register an invalid piece of data according to schema."
   [& args]
-  (let [[path args] (if (keyword? (first args)) [nil args] [(first args) (rest args)])
+  (let [[path args] (if (keyword? (first args))
+                      [nil args] [(first args) (rest args)])
         key (first args)
         data (second args)]
     `(let [error# {:ref ~path :key ~key :data ~data}]
@@ -56,7 +58,6 @@
          (.add errors# (merge {:path error-path#} error#)))
        (process-errors (list error#)))))
 
-
 (defmacro report-errors
   "Returns all errors, instead of simple boolean."
   [& args]
@@ -64,28 +65,26 @@
      (with-validation-context
        (do ~@args))))
 
-
 (defmulti validate*
-  "Dispatch on object type for validation. If not implemented, performs only basic type validation. Users can extend which types are supported by implementing validation for new types."
+  "Dispatch on object type for validation. If not implemented, performs
+  only basic type validation. Users can extend which types are supported
+  by implementing validation for new types."
   (fn [schema instance]
     (if (:enum schema)
       "enum"
       (:type schema))))
 
-
 (defn validate
-  "Entry point. A validation context is created and validation is dispatched to the appropriated multimethod."
+  "Entry point. A validation context is created and
+  validation is dispatched to the appropriated multimethod."
   [schema instance]
   (with-validation-context
     (validate* schema instance)))
 
-
 (def default-type "object")
-
 
 (defmethod validate* nil [schema instance]
   (validate (merge schema {:type default-type}) instance))
-
 
 (def ^{:doc "Known basic types."}
      basic-type-validations
@@ -98,17 +97,16 @@
        "null" #(nil? %)
        "any" (fn [] true)})
 
-
 (defn check-basic-type
   "Validate basic type definition for known types."
   [{t :type :as schema} instance]
-
 
   (if (and (nil? instance) (:optional schema))
     true
 
     (let [t (or t default-type)
           types (if (coll? t) t (vector t))]
+
       (or (reduce #(or %1 %2)
                   (map (fn [t] ((basic-type-validations t) instance)) types))
           (invalid :type {:expected types :actual (type instance)})))))
@@ -122,10 +120,8 @@
            extends)
   )
 
-
 (defmethod validate* :default [schema instance]
   (common-validate schema instance))
-
 
 (defmethod validate* "object"
   [{properties-schema :properties
@@ -141,12 +137,12 @@
       (when-not (or property optional)
         (invalid property-name :required))))
 
-
-  #_ "validate instance properties (using invidivual or addicional schema)"
+  #_ "validate instance properties (using invidivual or additional schema)"
 
   (doseq [[property-name property] instance]
     (if-let [{requires :requires :as property-schema}
-             (or (and (map? properties-schema) (properties-schema property-name))
+             (or (and (map? properties-schema)
+                      (properties-schema property-name))
                  (and (map? additional-schema) additional-schema))]
       (do
         (when (and requires property
@@ -165,10 +161,8 @@
     (if-let [additionals (set/difference (set (keys instance))
                                       (set (keys properties-schema)))]
       (when (> (count additionals) 0)
-        (invalid :addicional-properties-not-allowed {:properties additionals})))))
-
-
-
+        (invalid :addicional-properties-not-allowed
+                 {:properties additionals})))))
 
 (defmethod validate* "array"
   [{items-schema :items
@@ -202,7 +196,9 @@
 
                            (coll? items-schema)
                            (merge schema {:type "object"
-                                          :properties (zipmap (range (count items-schema)) items-schema)}))]
+                                          :properties
+                                          (zipmap (range (count items-schema))
+                                                  items-schema)}))]
       (validate obj-schema obj-array))))
 
 
@@ -214,20 +210,24 @@
 
   (when (schema :maxLength)
     (if-not (>= (schema :maxLength) (count instance))
-      (invalid :max-length-exceeded {:maxLength (schema :maxLength) :actual (count instance) })))
+      (invalid :max-length-exceeded
+               {:maxLength (schema :maxLength) :actual (count instance) })))
 
   (when (schema :minLength)
     (if-not (<= (schema :minLength) (count instance))
-      (invalid :min-length-not-reached {:minLength (schema :minLength) :actual (count instance) })))
+      (invalid :min-length-not-reached
+               {:minLength (schema :minLength) :actual (count instance) })))
 
   (when (schema :pattern)
     (if-not (.matches ^String instance (schema :pattern))
-      (invalid :pattern-not-matched {:pattern (schema :pattern) :actual instance}))))
+      (invalid :pattern-not-matched
+               {:pattern (schema :pattern) :actual instance}))))
 
 (defmethod validate* "enum"
   [schema instance]
   (if-not (true? (some #(= % instance) (schema :enum)))
-    (invalid :value-not-in-enum {:enum (schema :enum) :value instance })))
+    (invalid :value-not-in-enum
+             {:enum (schema :enum) :value instance })))
 
 
 (defmethod validate* "number"
@@ -235,24 +235,31 @@
   (common-validate schema instance)
   (when (schema :maximum)
    (if-not (> (schema :maximum) instance)
-    (invalid :value-lower-them-maximum {:maximum (schema :maximum) :value instance })))
+    (invalid :value-lower-them-maximum
+             {:maximum (schema :maximum) :value instance })))
 
   (when (schema :minimum)
    (if-not (< (schema :minimum) instance)
-    (invalid :value-lower-them-minimum {:minimum (schema :minimum) :value instance })))
+    (invalid :value-lower-them-minimum
+             {:minimum (schema :minimum) :value instance })))
 
   (when (schema :maximumCanEqual)
    (if-not (>= (schema :maximumCanEqual) instance)
-    (invalid :value-lower-them-maximumCanEqual {:maximumCanEqual (schema :maximumCanEqual) :value instance })))
+    (invalid :value-lower-them-maximumCanEqual
+             {:maximumCanEqual (schema :maximumCanEqual)
+              :value instance })))
 
   (when (schema :minimumCanEqual)
    (if-not (<= (schema :minimumCanEqual) instance)
-    (invalid :value-lower-them-minimumCanEqual {:minimumCanEqual (schema :minimumCanEqual) :value instance })))
+    (invalid :value-lower-them-minimumCanEqual
+             {:minimumCanEqual (schema :minimumCanEqual) :value instance })))
 
   (when (schema :divisibleBy)
    (if-not (= 0 (mod instance (schema :divisibleBy)))
-    (invalid :value-not-divisible-by {:divisibleBy (schema :divisibleBy) :value instance})))
+    (invalid :value-not-divisible-by
+             {:divisibleBy (schema :divisibleBy) :value instance})))
   )
 
-
-
+;; Test
+;; (validate {:type "array" :items {:type "string"}} ["1" "2"])
+;; (validate {:type "array" :items {:type "string"}} ["1" 2])
